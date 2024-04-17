ba5664a3d8861bffc0e47b995e434921c01cc22c
AdharshKSaji <adharshksaji001@gmail.com>
2024-04-17 13:29:19 +0530

    first commit

---
+++ lib/controller/homescreencontroller.dart
@@ -0,0 +1,53 @@
+
+import 'dart:convert';
+import 'package:api_wen/model/apimodel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreenController with ChangeNotifier {
+  bool isloading = false;
+  String baseurl = "http://3.93.46.140";
+  List<Employee>? employeeslist = [];
+
+  Future getEmployee() async {
+    isloading = true;
+    notifyListeners();
+
+    Uri url = Uri.parse(baseurl + "/employees/");
+
+    var res = await http.get(url);
+
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      var decodedata = jsonDecode(res.body);
+      Employeeresmodel resmodel = Employeeresmodel.fromJson(decodedata);
+      employeeslist = resmodel.employeeslist ?? [];
+    }
+    isloading = false;
+    notifyListeners();
+  }
+
+  Future Deleteemployee(var id) async {
+    isloading = true;
+    notifyListeners();
+
+    Uri url = Uri.parse(baseurl + "/employees/$id/");
+
+    var res = await http.delete(url);
+
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      await getEmployee();
+    }
+    isloading = false;
+    notifyListeners();
+  }
+  Future addData()async{
+     Uri url = Uri.parse(baseurl + "/employees/create/");
+
+    var res = await http.post(url,body:{ "name":"adharsh","role":"flutter developer"});
+
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      await getEmployee();
+    }
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:api_wen/controller/homescreencontroller.dart';
+import 'package:api_wen/view/homescrren/homesceen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [ChangeNotifierProvider(create: (context) => HomeScreenController()),
+    
+   ] ,child: MaterialApp(home: HomeScreen()));
+  }
+}
